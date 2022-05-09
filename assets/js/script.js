189548d7f059d7dc0bdb62ab2517cdb5189fd7c7
Dennis <denduplon31@gmail.com>
2022-05-09 16:57:17 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const ingredientsBtn = document.querySelector('.ingredients-button');
+
+btnHamburger.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  } else {
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }
+});
+
+if (window.innerWidth <= 428) {
+  ingredientsBtn.innerHTML =
+    'GET PURPOSE NOW <i class="fa-solid fa-play ms-3"></i>';
+}
+
+ingredientsBtn.addEventListener('click', e => {
+  console.log(e);
+});

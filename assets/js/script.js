509a4515b921f64d8879285975c40d61db2e8af4
coffee-sprinkler <dennisduplon.ifliba@gmail.com>
2022-05-11 10:33:49 +0800

    fix resize

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,74 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
-const ingredientsBtn = document.querySelector('.ingredients-button');
+const btnHamburger = document.querySelector('#btnHamburger')
+const body = document.querySelector('body')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const ingredientsBtn = document.querySelector('.ingredients-button')
 
 btnHamburger.addEventListener('click', function () {
   if (header.classList.contains('open')) {
-    body.classList.remove('noscroll');
-    header.classList.remove('open');
+    body.classList.remove('noscroll')
+    header.classList.remove('open')
     fadeElems.forEach(function (element) {
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
-    });
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
   } else {
-    body.classList.add('noscroll');
-    header.classList.add('open');
+    body.classList.add('noscroll')
+    header.classList.add('open')
     fadeElems.forEach(function (element) {
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
+      element.classList.remove('fade-out')
+      element.classList.add('fade-in')
+    })
   }
-});
+})
 
-const bundleItems = document.querySelectorAll('.bundle-item');
-const rightButton = document.querySelector('[data-right]');
-const leftButton = document.querySelector('[data-left]');
+const bundleItems = document.querySelectorAll('.bundle-item')
+const rightButton = document.querySelector('[data-right]')
+const leftButton = document.querySelector('[data-left]')
 
-const MOBILE = 428;
+const MOBILE = 428
 
 const showCard = steps => {
   bundleItems.forEach((element, value) => {
     if (value == steps) {
-      element.classList.add('show');
+      element.classList.add('show')
     } else {
-      element.classList.remove('show');
+      element.classList.remove('show')
     }
-  });
-};
+  })
+}
 
 const mobileView = () => {
   ingredientsBtn.innerHTML =
-    'GET PURPOSE NOW <i class="fa-solid fa-play ms-3"></i>';
+    'GET PURPOSE NOW <i class="fa-solid fa-play ms-3"></i>'
 
-  let steps = 0;
+  let steps = 0
   const moveRight = rightButton.addEventListener('click', () => {
     if (steps < 2) {
-      steps++;
+      steps++
     } else {
-      steps = 0;
+      steps = 0
     }
-    showCard(steps);
-  });
+    showCard(steps)
+  })
   const moveLeft = leftButton.addEventListener('click', () => {
     if (steps == 0) {
-      steps = 2;
+      steps = 2
     } else {
-      steps--;
+      steps--
     }
-    showCard(steps);
-  });
-};
+    showCard(steps)
+  })
+}
 
 const webView = () => {
   ingredientsBtn.innerHTML = `View Our Full List of Ingredients
-  <img class="new__window ms-3" src="./assets/images/new-window.png" alt="new-window">`;
-};
-
-if (window.innerWidth <= MOBILE) {
-  mobileView();
-} else {
-  webView();
+  <img class="new__window ms-3" src="./assets/images/new-window.png" alt="new-window">`
 }
+
+window.innerWidth <= MOBILE ? mobileView() : webView()
+
+window.addEventListener('resize', () => {
+  window.innerWidth <= MOBILE ? mobileView() : webView()
+})
